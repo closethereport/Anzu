e7eb09e7c1f93d5ab73226fbf84919d5cae95c98
NeluQi <nelunull@gmail.com>
2019-06-02 21:05:11 +0300

    New

--- Anzu/MainForm.cs
+++ Anzu/MainForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MetroFramework.Forms;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,11 +11,15 @@ using System.Windows.Forms;
 
 namespace Anzu
 {
-	public partial class Form1 : Form
+	public partial class MainForm : MetroForm
 	{
-		public Form1()
+		public MainForm()
 		{
 			InitializeComponent();
+		}
+
+		private void MainForm_Load(object sender, EventArgs e)
+		{
 		}
 	}
 }
